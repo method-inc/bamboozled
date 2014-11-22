2624aca2efc38c1ade224baf5f8a87df65bfa8d4
Christopher Reynoso <topherreynoso@gmail.com>
2014-11-22 01:07:07 -0700

    added login support for app key

--- lib/bamboozled.rb
+++ lib/bamboozled.rb
@@ -6,6 +6,7 @@ require_relative './bamboozled/ext/yesno'
 
 require_relative './bamboozled/version'
 require_relative './bamboozled/errors'
+require_relative './bamboozled/auth'
 require_relative './bamboozled/base'
 
 %w(base employee report time_off meta).each {|a| require_relative "./bamboozled/api/#{a}"}
@@ -15,6 +16,11 @@ module Bamboozled
     # Creates a standard client that will raise all errors it encounters
     def client(subdomain: nil, api_key: nil)
       Bamboozled::Base.new(subdomain: subdomain, api_key: api_key)
+    end
+
+    # Creates an auth client that will raise all errors it encounters
+    def auth(app_key: nil)
+      Bamboozled::Auth.new(app_key: app_key)
     end
   end
 end
