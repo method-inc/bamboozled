d137540c6292dcbc81b88f1e5e8af5b777dec1d3
Christopher Reynoso <topherreynoso@gmail.com>
2014-11-23 22:46:10 -0500

    fixed meta fields to return with xml formatting, not json

--- lib/bamboozled/api/meta.rb
+++ lib/bamboozled/api/meta.rb
@@ -3,9 +3,8 @@ module Bamboozled
     class Meta < Base
 
       [:field, :table, :list, :user].each do |action|
-        define_method("#{action}s") do
-          result = request(:get, "meta/#{action}s")
-          result["#{action}s".to_sym][action]
+        define_method(action.to_s}+"s") do
+          request(:get, "meta/#{action}s", {:tabular => true})
         end
       end
 
