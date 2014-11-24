e3428d57843f460337dbaf72fd3509f2d7355ed3
Christopher Reynoso <topherreynoso@gmail.com>
2014-11-23 22:54:42 -0500

    fixed typo in meta

--- lib/bamboozled/api/meta.rb
+++ lib/bamboozled/api/meta.rb
@@ -3,8 +3,8 @@ module Bamboozled
     class Meta < Base
 
       [:field, :table, :list, :user].each do |action|
-        define_method(action.to_s}+"s") do
-          request(:get, "meta/#{action}s", {:tabular => true})
+        define_method(action.to_s+"s") do
+          request(:get, "meta/"+action.to_s+"s", {:tabular => true})
         end
       end
 
