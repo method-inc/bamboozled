bf2669977046e0ff5fbbade16628bcc783f597eb
Jonathan Hoyt <hoyt@github.com>
2017-06-07 15:12:32 -0700

    Update Bamboozled::API::Meta to match API responses from Bamboo

--- lib/bamboozled/api/meta.rb
+++ lib/bamboozled/api/meta.rb
@@ -2,13 +2,39 @@ module Bamboozled
   module API
     class Meta < Base
 
-      [:field, :table, :list, :user].each do |action|
-        define_method("#{action}s") do
-          result = request(:get, "meta/#{action}s")
-          result["#{action}s".to_sym][action]
-        end
+      # Public: Get a list of fields.
+      # See https://www.bamboohr.com/api/documentation/metadata.php#getFields
+      #
+      # Returns an Array.
+      def fields
+        request(:get, "meta/fields")
       end
 
+      # Public: Get the details for "list" fields in an account.
+      # See https://www.bamboohr.com/api/documentation/metadata.php#getLists
+      #
+      # Returns an Array.
+      def lists
+        result = request(:get, "meta/lists")
+        result[:lists][:list]
+      end
+
+      # Public: Get a list of tabular fields.
+      # See https://www.bamboohr.com/api/documentation/metadata.php#getTables
+      #
+      # Returns an Array.
+      def tables
+        result = request(:get, "meta/tables")
+        result[:tables][:table]
+      end
+
+      # Public: Get a list of users.
+      # See https://www.bamboohr.com/api/documentation/metadata.php#getUsers
+      #
+      # Returns an Array.
+      def users
+        request(:get, "meta/users").values
+      end
     end
   end
 end
